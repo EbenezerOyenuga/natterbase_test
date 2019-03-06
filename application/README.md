c2ec777c68c02508d0f56f90466e175cabf0707f
EbenezerOyenuga <ebene.oyen@yahoo.com>
2019-03-06 11:31:23 +0100

    Database Added

---
+++ application/README.md
@@ -0,0 +1,33 @@
+# natterbase_test
+This is a RESTFUL API that uses jwt for authentication, built with CodeIgniter for performing CRUD operations on a Country resource. Though users need to be registered first. The api also logs the activities of the user. The user can view the activities, which shows five per page
+
+#1 /Signup (POST)
+Requires the parameters below:
+1. firstname
+2. lastname
+3. date_of_birth (Date of birth should be in this format "yyyy-mm-dd")
+4. email
+5. username
+6.password
+
+#2 /login (POST)
+Login with the parameters below:
+1. username
+2. password
+It generates a token. Copy the token into the header you are using to test the API, using the settings below:
+Key - Authorization
+Value - [TOKEN_GENERATED]
+
+#3 /countries (POST)
+Creates Country. Requires the parameters below
+1. name
+2. continent
+
+#4 /countries (GET)
+Gets all the countries
+
+#5 /countries/(:num)/delete
+Delete Country with the id
+
+#6 /activities/(:num)
+Gets all the activities. The num is the page number. It gets five activities per page
---
+++ natterbase.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 5.6.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: natterbase_test
+-- ------------------------------------------------------
+-- Server version	5.6.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `activities`
+--
+
+DROP TABLE IF EXISTS `activities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `activity` varchar(255) NOT NULL,
+  `date_of_activity` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `activities`
+--
+
+LOCK TABLES `activities` WRITE;
+/*!40000 ALTER TABLE `activities` DISABLE KEYS */;
+INSERT INTO `activities` VALUES (26,6,'Tried Viewing Activities but no activities found','2019-03-06 10:45:52'),(27,6,'Successfully Viewed all Activities','2019-03-06 10:45:58'),(28,6,'Tried Viewing Countries but no countries found','2019-03-06 10:47:43'),(29,6,'Successfully Viewed all Activities','2019-03-06 10:47:50'),(30,6,'Successfully Viewed all Activities','2019-03-06 10:48:05'),(31,6,'Successfully Viewed all Activities','2019-03-06 10:48:06'),(32,6,'Successfully Viewed all Activities','2019-03-06 10:48:08'),(33,6,'Successfully Viewed all Activities','2019-03-06 10:48:12'),(34,6,'Successfully Viewed all Activities','2019-03-06 10:48:18');
+/*!40000 ALTER TABLE `activities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `continent` varchar(45) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `countries`
+--
+
+LOCK TABLES `countries` WRITE;
+/*!40000 ALTER TABLE `countries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `countries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-06 11:28:53
